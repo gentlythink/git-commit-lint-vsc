c28f9e737b29dfb09c6eb6af5c6ba25aad311987
keming <keming.li@filhub.com>
2022-11-08 15:38:59 +0800

    fix:  update complie out config files

--- out/config/git_emoji_zh.js
+++ out/config/git_emoji_zh.js
@@ -2,91 +2,91 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let emojis = [
     {
-        emoji: "feat✨: ",
+        emoji: "feat: ",
         code: "feat: ",
         description: "引入新功能",
         name: "新功能"
     },
     {
-        emoji: "fix🐛: ",
+        emoji: "fix: ",
         code: "fix: ",
         description: "修复bug",
         name: "bug"
     },
     {
-        emoji: "style💄: ",
+        emoji: "style: ",
         code: "style: ",
         description: "更新UI样式文件",
         name: "样式"
     },
     {
-        emoji: "format🥚: ",
+        emoji: "format: ",
         code: "format: ",
         description: "格式化代码",
         name: "格式化"
     },
     {
-        emoji: "docs📝: ",
+        emoji: "docs: ",
         code: "docs: ",
         description: "添加/更新文档",
         name: "文档"
     },
     {
-        emoji: "perf👌: ",
+        emoji: "perf: ",
         code: "perf: ",
         description: "提高性能/优化",
         name: "优化"
     },
     {
-        emoji: "init🎉: ",
+        emoji: "init: ",
         code: "init: ",
         description: "初次提交/初始化项目",
         name: "初始化"
     },
     {
-        emoji: "test✅: ",
+        emoji: "test: ",
         code: "test: ",
         description: "增加测试代码",
         name: "测试"
     },
     {
-        emoji: "refactor🎨: ",
+        emoji: "refactor: ",
         code: "refactor: ",
         description: "改进代码结构/代码格式",
         name: "优化"
     },
     {
-        emoji: "patch🚑: ",
+        emoji: "patch: ",
         code: "patch: ",
         description: "添加重要补丁",
         name: "补丁"
     },
     {
-        emoji: "file📦: ",
+        emoji: "file: ",
         code: "file: ",
         description: "添加新文件",
         name: "新文件"
     },
     {
-        emoji: "publish🚀: ",
+        emoji: "publish: ",
         code: "publish: ",
         description: "发布新版本",
         name: "新版本"
     },
     {
-        emoji: "tag📌: ",
+        emoji: "tag: ",
         code: "tag: ",
         description: "发布版本/添加标签",
         name: "书签"
     },
     {
-        emoji: "config🔧: ",
+        emoji: "config: ",
         code: "config: ",
         description: "修改配置文件",
         name: "配置"
     },
     {
-        emoji: "git🙈: ",
+        emoji: "git: ",
         code: "git: ",
         description: "添加或修改.gitignore文件",
         name: "不可见"
